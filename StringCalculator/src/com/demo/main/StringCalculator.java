ff7bcd24e108fe05d0340237ba007c78c0764ae4
neha <singhsatyajeet25@gmail.com>
2021-01-13 00:21:59 +0530

    Case 3: Allow the Add method to handle new lines between numbers (instead of commas). the following input is ok: “1\n2,3” (will equal 6) the following input is NOT ok: “1,\n” (not need to prove it - just clarifying)

--- StringCalculator/src/com/demo/main/StringCalculator.java
+++ StringCalculator/src/com/demo/main/StringCalculator.java
@@ -1,10 +1,21 @@
 package com.demo.main;
 
 public class StringCalculator {
+
+	/*
+	 * Allow the Add method to handle new lines between numbers (instead of commas).
+	 * the following input is ok: “1\n2,3” (will equal 6) the following input is NOT
+	 * ok: “1,\n” (not need to prove it - just clarifying)
+	 */
 	public static int add(String numbers) {
 		// removing the delimiter and storing in a String array.
-		String[] numbersArray = numbers.split(",");
+		String[] numbersArray = numbers.split(",|\n");
 		int result = 0;
+		if(numbersArray.length <2)
+		{
+			throw new RuntimeException("Atleast 2 numbers are required");
+		}
+		
 		for (String number : numbersArray) {
 
 			// Checking whether input is valid number or not.
--- StringCalculator/src/com/demo/test/StringCalculatorTest.java
+++ StringCalculator/src/com/demo/test/StringCalculatorTest.java
@@ -8,14 +8,27 @@ import com.demo.main.StringCalculator;
 
 public class StringCalculatorTest {
 
-	//Allow the Add method to handle an unknown amount of numbers
+	//Allow the Add method to handle an unknown amount of numbers.
 	@Test
 	public final void whenNNumberIsTheirThenReturnSum() {
 		assertEquals(10, StringCalculator.add("1,2,3,4"));
 	}
 
+	//Allow to check the type of input.
 	@Test(expected = RuntimeException.class)
 	public final void whenInvalidNumberThenExceptionIsThrown() {
 		StringCalculator.add("W,A");
+	}
+	
+	//Allow to add numbers if input contains \n in it.
+	@Test
+	public final void whenNewLineThenReturnSum() {
+	    assertEquals(17, StringCalculator.add("12,4\n1"));
+	}
+	
+	//Throw Exception if only one number is Present.
+	@Test(expected = RuntimeException.class)
+	public final void whenOnlyOneCorrectValueIsPresentThenThrowRuntimeException() {
+		StringCalculator.add("1,\n");
 	}
 }
